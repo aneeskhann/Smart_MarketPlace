368634c03a5fb516fa1eaafcdb9049aa01c81b0e
Faizan <faizanswabi95@gmail.com>
2023-06-19 02:33:49 +0500

    design updates

--- frontend/src/components/Carousel/Slider.js
+++ frontend/src/components/Carousel/Slider.js
@@ -48,7 +48,7 @@ function Slider() {
   };
 
   return (
-    <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group rounded-2xl'>
+    <div className='max-w-[1400px] h-[600px] w-full m-auto py-12 px-4 relative group rounded-2xl'>
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
         className='w-full h-full rounded-2xl bg-center bg-cover duration-500 relative'
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -30,15 +30,19 @@ const Header = () => {
       name: "Contact",
       path: "/contact",
     },
+    {
+      name: "Sell-Item",
+      path: "/add"
+    }
   ];
 
   return (
-    <header className="text-black-600 body-font shadow-xl bg-white p-3">
+    <header className="text-black-600 body-font shadow-lg bg-white ">
       <div className="bg-red-600 text-end text-white p-1 text-sm">
         <marquee
           behavior="scroll"
           direction="left"
-          scrollamount="8"
+          scrollamount="6"
           className="font-sans"
         >
           All online operators are currently affected due to which there might
@@ -46,19 +50,23 @@ const Header = () => {
         </marquee>
       </div>
 
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <img src={logo} alt="logo" className="md:cursor-pointer h-14" />
+      <div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
+        <img src={logo} alt="logo" className="md:cursor-pointer h-10 ml-2 bg-white" />
+
         <Link
           to="/"
-          className="flex cursor-pointer title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+          className="flex cursor-pointer title-font font-medium items-center mb-4 md:mb-0 "
         >
-          <span className="ml-3 text-2xl">MŌCCIANI</span>
+          <span className="font-roboto text-2xl text-red-800">
+            WishAttire
+          </span>
         </Link>
+
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center cursor-pointer">
           {navigations.map((navigation) => (
             <Link
               to={navigation.path}
-              className="mr-5 text-lg font-sans hover:text-red-400 capitalize"
+              className="mr-5 text-sm font-sans text-red-600 hover:text-black capitalize"
               key={navigation.name}
             >
               {navigation.name}
@@ -74,22 +82,28 @@ const Header = () => {
 
         <Link
           to="./signin"
-          className="inline-flex items-center text-white bg-red-500 border-0 py-2 px-4 focus:outline-none hover:bg-red-600 rounded-full text-base mt-4 md:mt-0   transition duration-300"
+          className="inline-flex items-center text-white bg-red-500 border-0 py-1 px-3 focus:outline-none hover:bg-red-600 rounded-full text-base mt-4 md:mt-0  transition duration-300"
         >
           Sign in
         </Link>
-        <Badge badgeContent={carts.length} color="secondary" overlap="rectangular" >
-          <Link to={"/cart"}>
-            <svg
-              className="ml-5"
-              xmlns="http://www.w3.org/2000/svg"
-              height="25px"
-              viewBox="0 0 576 512"
-            >
-              <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
-            </svg>
-          </Link>
-        </Badge>
+        <div className="pr-5">
+          <Badge
+            badgeContent={carts.length}
+            color="secondary"
+            overlap="rectangular"
+          >
+            <Link to={"/cart"}>
+              <svg
+                className="mr-2 ml-5"
+                xmlns="http://www.w3.org/2000/svg"
+                height="25px"
+                viewBox="0 0 576 512"
+              >
+                <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+              </svg>
+            </Link>
+          </Badge>
+        </div>
       </div>
     </header>
   );
