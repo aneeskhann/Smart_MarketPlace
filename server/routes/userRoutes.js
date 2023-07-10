c87968364787ce8256aedf975636b05a56952b00
Faizan <faizanswabi95@gmail.com>
2023-07-10 06:25:01 +0500

    Authentication added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,15 +1,20 @@
 import express from 'express'
-import { login, signup, getUsers, deleteUser } from '../controllers/userController.js';
+import { login, signup, getUsers, deleteUser, updateUser } from '../controllers/userController.js';
+import { authenticateToken } from './authentication/Authentication.js';
 
 const router = express.Router();
+
+
 
 // Route for user signup
 router.post('/signup', signup);
 
 router.post('/login', login);
 
-router.get('/user', getUsers)
+router.get('/user', authenticateToken, getUsers)
 
 router.delete('/user/:userId', deleteUser)
+
+router.put('/user/:userId', updateUser)
 
 export default router
